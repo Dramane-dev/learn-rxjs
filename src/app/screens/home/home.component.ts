13277c4431e0ffef4d7a4818f229fb50600a15c7
Dramane <Dramane.dev@gmail.com>
2022-12-15 12:29:53 +0100

    add an observable to get data from api

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ITodos } from 'src/app/interfaces/ITodos';
+import { axiosInstance } from 'src/app/shared/axios/axiosInstance';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +9,20 @@ import { Observable } from 'rxjs';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  public todos: string[] = [];
+  public todos: ITodos[] = [];
+  public todos$ = new Observable<ITodos[]>((observer) => {
+    axiosInstance.get('todos')
+     .then((res) => {
+      const todos: ITodos[] = res.data;
+      observer.next(todos);
+      observer.complete();
+     })
+     .catch((error) => observer.error(error));
+  });
 
   constructor() { }
 
   ngOnInit(): void {
+    this.todos$.subscribe((_todos) => this.todos = _todos);
   }
-
 }
